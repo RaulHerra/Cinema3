346c529d1142e43c5fae0f811fc53e03a04d7993
JesuLopezGomez <jelopez.gom@gmail.com>
2023-11-21 08:15:32 +0100

    .

--- src/main/java/com/jacaranda/model/Task.java
+++ src/main/java/com/jacaranda/model/Task.java
@@ -1,6 +1,7 @@
 package com.jacaranda.model;
 
 
+import java.util.List;
 import java.util.Objects;
 
 import com.jacaranda.exception.TaskException;
@@ -9,6 +10,7 @@ import com.jacaranda.exception.TaskException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
